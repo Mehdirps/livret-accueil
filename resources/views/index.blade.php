d3f1021694aca768542aed5b50993842fe83312f
Mehdirps <mehdi.raposo77@gmail.com>
2024-03-05 09:47:54 +0100

    Initial commit

---
+++ resources/views/index.blade.php
@@ -0,0 +1,13 @@
+@extends('layouts.app')
+
+@section('app_title', 'Accueil')
+
+@section('content')
+    <div class="container">
+        <h1>Bienvenue sur le livret d'accueil</h1>
+        <p>Vous trouverez ici toutes les informations nécessaires pour bien démarrer votre expérience chez nous.</p>
+    </div>
+@endsection
+
+@section('footer_script')
+@endsection
---
+++ resources/views/layouts/app.blade.php
@@ -0,0 +1,33 @@
+<!doctype html>
+<html lang="fr">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>@yield('app_title') | Livret d'accueil</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet"
+          integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js"
+            integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz"
+            crossorigin="anonymous"></script>
+    <script src="https://code.jquery.com/jquery-3.7.1.min.js"
+            integrity="sha256-/JqT3SQfawRcv/BIHPThkBvs0OEvtFFmqPF/lYI/Cxo=" crossorigin="anonymous"></script>
+</head>
+<body>
+<header>
+    <nav class="navbar navbar-light bg-light">
+        <div class="container">
+            <a class="navbar-brand" href="#">Livret d'accueil</a>
+        </div>
+    </nav>
+</header>
+<main class="container">
+    @yield('content')
+</main>
+<footer class="text-center">
+    <p>Livret d'accueil - 2024</p>
+    @yield('footer_script')
+</footer>
+</body>
+</html>

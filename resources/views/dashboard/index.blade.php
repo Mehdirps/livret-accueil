720f29d8241328f8b74cb8c9294f2bf8d6d0dc6a
Mehdirps <mehdi.raposo77@gmail.com>
2024-03-07 16:08:10 +0100

    UP : Success alert

--- resources/views/dashboard/index.blade.php
+++ resources/views/dashboard/index.blade.php
@@ -1,16 +1,19 @@
 @extends('layouts.dashboard')
-@section('dasboard_title', 'Bienvenue sur le livret d\'accueil')
+@section('dashboard_title', 'Bienvenue sur le livret d\'accueil')
 
 @section('dashboard_content')
     <div class="container">
         <div class="row">
             <div class="col-12">
-                <h1>Bienvenue sur le livret d'accueil</h1>
+                <h1>Bienvenue sur votre livret d'accueil - <strong>{{$livret->livret_name}}</strong></h1>
                 <p>Vous êtes actuellement sur le livret d'accueil de votre entreprise. Vous pouvez consulter les
                     informations importantes de votre entreprise, les documents à lire, les formations à suivre,
                     etc.</p>
                 <p>Vous pouvez également consulter les statistiques de votre entreprise, les informations importantes,
                     etc.</p>
+                @if(session('success'))
+                    <div class="alert alert-success">{{ session('success') }}</div>
+                @endif
             </div>
         </div>
     </div>

291a0fc252eebd75eb09b31b20681dfecaf9be85
Mehdirps <mehdi.raposo77@gmail.com>
2024-03-07 16:31:58 +0100

    UP : Update user request

---
+++ app/Http/Requests/UpdateUserRequest.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace App\Http\Requests;
+
+use Illuminate\Foundation\Http\FormRequest;
+use Illuminate\Validation\Rule;
+
+class UpdateUserRequest extends FormRequest
+{
+    /**
+     * Determine if the user is authorized to make this request.
+     */
+    public function authorize(): bool
+    {
+        return true;
+    }
+
+    /**
+     * Get the validation rules that apply to the request.
+     *
+     * @return array<string, \Illuminate\Contracts\Validation\ValidationRule|array<mixed>|string>
+     */
+    public function rules(): array
+    {
+        return [
+            'civility' => ['required', Rule::in(['M.', 'Mme'])],
+            'last_name' => ['required', 'string', 'max:255'],
+            'email' => ['required', 'string', 'email', 'max:255', Rule::unique('users')->ignore($this->user()->id)],
+            'phone' => 'required|string|regex:/^(\+\d{1,3})?\d{7,14}$/',
+            'birth_date' => ['required', 'date'],
+            'address' => ['required', 'string', 'max:255'],
+        ];
+    }
+}

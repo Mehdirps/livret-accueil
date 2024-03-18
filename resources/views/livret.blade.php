22db46d1224c8bb50ccfe69273efd4231508def2
Mehdirps <mehdi.raposo77@gmail.com>
2024-03-18 09:48:10 +0100

    UP : show livret

--- resources/views/livret.blade.php
+++ resources/views/livret.blade.php
@@ -5,9 +5,30 @@
     <meta name="viewport"
           content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>{{$livret->livret_name}}</title>
+    <title>{{$livret->livret_name}} | Livret d'accueil</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet"
+          integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js"
+            integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz"
+            crossorigin="anonymous"></script>
+    <script src="https://code.jquery.com/jquery-3.7.1.min.js"
+            integrity="sha256-/JqT3SQfawRcv/BIHPThkBvs0OEvtFFmqPF/lYI/Cxo=" crossorigin="anonymous"></script>
 </head>
 <body>
-
+<main>
+    <div class="container py-5 text-center">
+        <div class="row justify-content-center">
+            <div class="col-lg-6">
+                <img src="{{ asset($livret->logo) }}" alt="Logo" style="width: 100px;" class="img-fluid rounded-circle mx-auto d-block">
+            </div>
+        </div>
+        <div class="row justify-content-center mt-4">
+            <div class="col-lg-6">
+                <h1 class="display-4">{{ $livret->livret_name }}</h1>
+                <p class="lead">{!! nl2br(e($livret->description)) !!}</p>
+            </div>
+        </div>
+    </div>
+</main>
 </body>
 </html>

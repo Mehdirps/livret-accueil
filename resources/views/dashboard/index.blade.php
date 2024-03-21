6ff2f1030a4ab091fa0941c0648538228c5619ba
Mehdirps <mehdi.raposo77@gmail.com>
2024-03-21 16:18:11 +0100

    Up : faq

--- resources/views/dashboard/index.blade.php
+++ resources/views/dashboard/index.blade.php
@@ -19,5 +19,85 @@
                 @endif
             </div>
         </div>
+        <div class="col-12">
+            <h2>Comment fonctionne l'application</h2>
+            <div class="accordion" id="faqAccordion">
+                <div class="accordion-item">
+                    <h3 class="accordion-header" id="headingOne">
+                        <button class="accordion-button" type="button" data-bs-toggle="collapse"
+                                data-bs-target="#collapseOne" aria-expanded="true" aria-controls="collapseOne">
+                            Comment modifier mes informations et celle de mon livret ?
+                        </button>
+                    </h3>
+                    <div id="collapseOne" class="accordion-collapse collapse show" aria-labelledby="headingOne"
+                         data-bs-parent="#faqAccordion">
+                        <div class="accordion-body">
+                            Pour modifier vos informations, vous devez vous rendre dans la section "Mon profil" de votre
+                            compte. Vous pouvez modifier votre nom, prénom, email, mot de passe, etc. Pour modifier les
+                            informations de votre livret, vous devez vous rendre dans la section "Mon profil". Vous
+                            pouvez modifier le nom de votre livret, la description, ajout un logo etc.
+                        </div>
+                    </div>
+                </div>
+                <div class="accordion-item">
+                    <h3 class="accordion-header" id="headingTwo">
+                        <button class="accordion-button collapsed" type="button" data-bs-toggle="collapse"
+                                data-bs-target="#collapseTwo" aria-expanded="false" aria-controls="collapseTwo">
+                            Comment changer le fond d'écran de mon livret ?
+                        </button>
+                    </h3>
+                    <div id="collapseTwo" class="accordion-collapse collapse" aria-labelledby="headingTwo"
+                         data-bs-parent="#faqAccordion">
+                        <div class="accordion-body">
+                            Pour changer le fond d'écran de votre livret, vous devez vous rendre dans la section "Mon
+                            livret d'accueil" de votre compte. Vous pouvez choisir un fond parmis les images
+                            disponibles. Plusieurs catégories d'images sont disponibles.
+                        </div>
+                    </div>
+                </div>
+                <div class="accordion-item">
+                    <h3 class="accordion-header" id="headingTwo">
+                        <button class="accordion-button collapsed" type="button" data-bs-toggle="collapse"
+                                data-bs-target="#collapseThree" aria-expanded="false" aria-controls="collapseThree">
+                            Comment fonctionne les statistiques ?
+                        </button>
+                    </h3>
+                    <div id="collapseThree" class="accordion-collapse collapse" aria-labelledby="headingTwo"
+                         data-bs-parent="#faqAccordion">
+                        <div class="accordion-body">
+                            Les statistiques vous permettent de voir le nombre de vue de votre livret en vous rendant dans la section "Statistiques" de votre compte. Vous pouvez voir le nombre de vues par jour, par semaine, par mois, et sélectionner une période personnalisée.
+                        </div>
+                    </div>
+                </div>
+                <div class="accordion-item">
+                    <h3 class="accordion-header" id="headingTwo">
+                        <button class="accordion-button collapsed" type="button" data-bs-toggle="collapse"
+                                data-bs-target="#collapseFoor" aria-expanded="false" aria-controls="collapseFoor">
+                            Comment voir mon livret en tant qu'utilisateur ?
+                        </button>
+                    </h3>
+                    <div id="collapseFoor" class="accordion-collapse collapse" aria-labelledby="headingTwo"
+                         data-bs-parent="#faqAccordion">
+                        <div class="accordion-body">
+                            Pour voir votre livret en tant qu'utilisateur, vous devez vous rendre dans la section "Mon livret d'accueil" de votre compte et cliquer sur "voir"..
+                        </div>
+                    </div>
+                </div>
+                <div class="accordion-item">
+                    <h3 class="accordion-header" id="headingTwo">
+                        <button class="accordion-button collapsed" type="button" data-bs-toggle="collapse"
+                                data-bs-target="#collapseFive" aria-expanded="false" aria-controls="collapseFive">
+                            Comment nous contacter ?
+                        </button>
+                    </h3>
+                    <div id="collapseFive" class="accordion-collapse collapse" aria-labelledby="headingTwo"
+                         data-bs-parent="#faqAccordion">
+                        <div class="accordion-body">
+                            Pour nous contacter, rien de plus simple. Cliquez sur le bouton "Nous contacter" en bas de la page et remplissez le formulaire de contact. Nous vous recontacterons dans les plus brefs délais.
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
     </div>
 @endsection

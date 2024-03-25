358545c926e5698b94b7f32e5a5728c63cc29e16
Mehdirps <mehdi.raposo77@gmail.com>
2024-03-25 09:49:48 +0100

    UP : active to users table

--- database/migrations/2024_03_25_084911_add_active_to_users_table.php
+++ database/migrations/2024_03_25_084911_add_active_to_users_table.php
@@ -12,7 +12,7 @@ return new class extends Migration
     public function up(): void
     {
         Schema::table('users', function (Blueprint $table) {
-            $table->string('avatar')->nullable();
+            $table->boolean('active')->default(1);
         });
     }
 
@@ -22,7 +22,7 @@ return new class extends Migration
     public function down(): void
     {
         Schema::table('users', function (Blueprint $table) {
-            $table->dropColumn('avatar');
+            $table->dropColumn('active');
         });
     }
 };

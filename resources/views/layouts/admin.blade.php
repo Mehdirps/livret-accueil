1531ef3eed9b0a6820e2b7d7ae575c65a9bc87ca
Mehdirps <mehdi.raposo77@gmail.com>
2024-03-25 09:53:18 +0100

    UP : session message

--- resources/views/layouts/admin.blade.php
+++ resources/views/layouts/admin.blade.php
@@ -60,6 +60,18 @@
     </div>
     <div class="col-md-9 col-10">
         <main>
+            @if(session('success'))
+                <div class="alert alert-success alert-dismissible fade show" role="alert">
+                    {{session('success')}}
+                    <button type="button" class="btn-close" data-bs-dismiss="alert" aria-label="Close"></button>
+                </div>
+            @endif
+            @if(session('error'))
+                <div class="alert alert-danger alert-dismissible fade show" role="alert">
+                    {{session('error')}}
+                    <button type="button" class="btn-close" data-bs-dismiss="alert" aria-label="Close"></button>
+                </div>
+            @endif
             @yield('admin_content')
         </main>
         <footer>

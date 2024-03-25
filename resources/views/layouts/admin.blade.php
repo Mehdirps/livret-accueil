b492390d36f62f884ebf9d1698eef450e5b06adb
Mehdirps <mehdi.raposo77@gmail.com>
2024-03-25 10:06:42 +0100

    UP : jquery script

--- resources/views/layouts/admin.blade.php
+++ resources/views/layouts/admin.blade.php
@@ -80,6 +80,8 @@
             @yield('admin_content')
         </main>
         <footer>
+            <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js"></script>
+            <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js"></script>
             <div class="container">
                 <div class="row">
                     <div class="col-12">

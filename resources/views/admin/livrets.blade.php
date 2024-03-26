6562996a58d31e793712f0d005ca5de8c2ee64df
Mehdirps <mehdi.raposo77@gmail.com>
2024-03-26 17:07:20 +0100

    UP : if for no livrets message

--- resources/views/admin/livrets.blade.php
+++ resources/views/admin/livrets.blade.php
@@ -11,11 +11,12 @@
                     @csrf
                     @method('GET')
                     <div class="input-group mb-3">
-                        <input type="text" class="form-control" placeholder="Rechercher un livret" aria-label="Rechercher un livret" aria-describedby="button-addon2" name="search">
+                        <input type="text" class="form-control" placeholder="Rechercher un livret"
+                               aria-label="Rechercher un livret" aria-describedby="button-addon2" name="search">
                         <button class="btn btn-outline-secondary" type="submit" id="button-addon2">Rechercher</button>
                     </div>
                 </form>
-               {{-- <a href="{{route('admin.livrets.create')}}" class="btn btn-primary">Ajouter un livret</a>--}}
+                {{-- <a href="{{route('admin.livrets.create')}}" class="btn btn-primary">Ajouter un livret</a>--}}
                 <div class="table-responsive">
                     <table class="table table-striped">
                         <thead>
@@ -30,30 +31,38 @@
                         </tr>
                         </thead>
                         <tbody>
-                        @foreach($livrets as $livret)
+                        @if(count($livrets) > 0)
+                            @foreach($livrets as $livret)
+                                <tr>
+                                    <td>{{$livret->livret_name}}</td>
+                                    <td>{{$livret->description}}</td>
+                                    <td>{{$livret->establishment_name}}</td>
+                                    <td>{{$livret->establishment_address}}</td>
+                                    <td>{{$livret->establishment_phone}}</td>
+                                    <td>
+                                        <a class="btn btn-primary"
+                                           href="{{route('livret.show',[$livret->slug, $livret->id])}}"> <i
+                                                class="bi bi-eye"></i></a>
+                                        <button type="button" class="btn btn-warning" data-toggle="modal"
+                                                data-target="#updateLivretModal_{{$livret->id}}">
+                                            <i class="bi bi-pen"></i>
+                                        </button>
+                                        {{-- <a href="{{route('admin.livrets.edit', $livret->id)}}"
+                                            class="btn btn-warning">Modifier</a>
+                                         <form action="{{route('admin.livrets.destroy', $livret->id)}}" method="post"
+                                               class="d-inline">
+                                             @csrf
+                                             @method('DELETE')
+                                             <button type="submit" class="btn btn-danger">Supprimer</button>
+                                         </form>--}}
+                                    </td>
+                                </tr>
+                            @endforeach
+                        @else
                             <tr>
-                                <td>{{$livret->livret_name}}</td>
-                                <td>{{$livret->description}}</td>
-                                <td>{{$livret->establishment_name}}</td>
-                                <td>{{$livret->establishment_address}}</td>
-                                <td>{{$livret->establishment_phone}}</td>
-                                <td>
-                                    <a class="btn btn-primary" href="{{route('livret.show',[$livret->slug, $livret->id])}}"> <i class="bi bi-eye"></i></a>
-                                    <button type="button" class="btn btn-warning" data-toggle="modal"
-                                            data-target="#updateLivretModal_{{$livret->id}}">
-                                        <i class="bi bi-pen"></i>
-                                    </button>
-                                   {{-- <a href="{{route('admin.livrets.edit', $livret->id)}}"
-                                       class="btn btn-warning">Modifier</a>
-                                    <form action="{{route('admin.livrets.destroy', $livret->id)}}" method="post"
-                                          class="d-inline">
-                                        @csrf
-                                        @method('DELETE')
-                                        <button type="submit" class="btn btn-danger">Supprimer</button>
-                                    </form>--}}
-                                </td>
+                                <td colspan="7" class="text-center">Aucun livret trouvé</td>
                             </tr>
-                        @endforeach
+                        @endif
                         </tbody>
                     </table>
                     @include('inc.pagination', ['paginator' => $livrets])
